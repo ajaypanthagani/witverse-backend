e84a2773adb1f6c49108a13119010ac2e53e2ff2
ajaypanthagani <ajaypanthagani321@gmail.com>
2020-08-04 10:56:57 +0530

    installed passport and other dependencies

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+/*routers*/
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
