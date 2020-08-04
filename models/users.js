f72750df612a72230f57cb342ba99bc7e54472d9
ajaypanthagani <ajaypanthagani321@gmail.com>
2020-08-04 11:35:30 +0530

    created userModel

---
+++ models/users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const LocalMongoose = require('passport-local-mongoose');
+
+
+const users = new Schema({
+
+    firstname : {
+
+        type : String,
+        required : true
+    },
+
+    lastname : {
+
+        type : String,
+        required : true
+    },
+
+    displayImage : {
+
+        type: String,
+        default : '/images/profile/default.png'
+    }
+},
+{
+    timestamps : true
+});
+
+users.plugin(LocalMongoose);
+
+const userModel = mongoose.model('User', users);
+
+module.exports = userModel;
