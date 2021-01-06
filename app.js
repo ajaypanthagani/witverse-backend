a3ff3562f2d5dd760f2fbf916a7046188ce7ec95
ajaypanthagani <ajaypanthagani321@gmail.com>
2021-01-06 22:02:14 +0530

    updated error handler

--- app.js
+++ app.js
@@ -74,9 +74,6 @@ app.use(function(req, res, next) {
 
 /* error handler*/
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // respond with an error status
   res.status(err.status || 500);
